1af83e5d4c5149fc03bf9142c65172208dce3bdb
Adam Wulf <adam.wulf@gmail.com>
2021-05-19 00:27:18 -0500

    Draft of project to startup on command line and connect to notion api

--- hunch/main.swift
+++ hunch/main.swift
@@ -6,6 +6,35 @@
 //
 
 import Foundation
+import ArgumentParser
 
 print("Hello, World!")
 
+struct Hunch: ParsableCommand {
+    @Argument() var token: String
+
+    func run() {
+        print(token)
+
+        guard let url = URL(string: "https://api.notion.com/v1/databases") else { fatalError() }
+        let session = URLSession(configuration: .ephemeral)
+        var req = URLRequest(url: url)
+        req.addValue("Bearer " + token, forHTTPHeaderField: "Authorization")
+
+
+        let task = session.dataTask(with: req) { data, response, error in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+            exit()
+        }
+
+        task.resume()
+        CFRunLoopRun()
+    }
+
+    func exit() {
+        CFRunLoopStop(CFRunLoopGetMain())
+    }
+}
+
+Hunch.main()
