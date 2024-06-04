c49c01f14a7ba0be30e670102cbb3793e8181af4
Adam Wulf <adam@adamwulf.me>
2024-06-04 14:56:14 -0500

    Better emoji parsing

--- hunch/Model.swift
+++ hunch/Model.swift
@@ -65,7 +65,7 @@ struct Database: Codable{
     var properties: [String: Property]
 
     var plainTextTitle: String {
-        let emoji = icon.map({ $0.emoji + " " }) ?? ""
+        let emoji = icon?.emoji.map({ $0 + " " }) ?? ""
         return emoji + title.reduce("", { $0 + $1.plainText })
     }
 
@@ -81,7 +81,23 @@ struct Database: Codable{
 
 struct Icon: Codable {
     var type: String
-    var emoji: String
+    var emoji: String?
+    var file: File?
+    var external: External?
+
+    struct File: Codable {
+        var url: String
+        var expiryTime: Date?
+
+        enum CodingKeys: String, CodingKey {
+            case url
+            case expiryTime = "expiry_time"
+        }
+    }
+
+    struct External: Codable {
+        var url: String
+    }
 }
 
 struct Link: Codable {
