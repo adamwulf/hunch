924ce913fabedf6451bf0ab04f4f8473bb9d1f59
Adam Wulf <adam@adamwulf.me>
2024-06-04 14:41:33 -0500

    Simple plain text title for databases

--- hunch/Model.swift
+++ hunch/Model.swift
@@ -60,13 +60,20 @@ struct Database: Codable{
     var id: String
     let created: Date
     var lastEdited: Date
+    var icon: Icon?
     var title: [RichText]
     var properties: [String: Property]
+
+    var plainTextTitle: String {
+        let emoji = icon.map({ $0.emoji + " " }) ?? ""
+        return emoji + title.reduce("", { $0 + $1.plainText })
+    }
 
     enum CodingKeys: String, CodingKey {
         case id
         case created = "created_time"
         case lastEdited = "last_edited_time"
+        case icon
         case title
         case properties
     }
