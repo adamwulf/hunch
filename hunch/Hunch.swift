a4d8f46ac234f24b3a56191b09b9ab4792b5c7db
Adam Wulf <adam@adamwulf.me>
2024-06-23 00:17:07 -0500

    simpler api

--- hunch/Hunch.swift
+++ hunch/Hunch.swift
@@ -39,60 +39,94 @@ struct Fetch: ParsableCommand {
         abstract: "Fetch pages or databases from Notion"
     )
 
-    func run() {
-        let group = DispatchGroup() // initialize
+    @Argument var entity: Entity
 
+    @Argument var entityId: String?
+
+    enum Entity: String, ExpressibleByArgument {
+        case database
+        case page
+    }
+
+    func run() {
+        log(.debug, "notion_api", context: ["action": "fetch_db"])
+
+        let group = DispatchGroup() // initialize
         group.enter()
 
-        log(.debug, "notion_api", context: ["action": "fetch_db"])
-        NotionAPI.shared.fetchDatabases { result in
-            switch result {
-            case .success(let dbs):
-                for db in dbs.results {
-                    group.enter()
-                    log(.debug, "notion_api", context: ["action": "fetch_entries", "db": db.plainTextTitle])
-                    NotionAPI.shared.fetchDatabaseEntries(in: db) { result in
-                        switch result {
-                        case .success(let pages):
-                            for page in pages.results {
-                                print(page.plainTextTitle)
-                            }
-                            if let firstPage = pages.results.first {
-                                group.enter()
-                                NotionAPI.shared.fetchPageContent(in: firstPage) { result in
-                                    switch result {
-                                    case .success(let blocks):
-                                        print(blocks)
-                                    case .failure(let error):
-                                        print(error)
-                                    }
-                                    group.leave()
-                                }
-                            }
-                        case .failure(let error):
-                            print(error)
-                        }
-                        group.leave()
+        switch entity {
+        case .database:
+            NotionAPI.shared.fetchDatabases { result in
+                switch result {
+                case .success(let dbs):
+                    print("\(dbs.object)");
+                    for db in dbs.results {
+                        print("\(db.id)")
                     }
-                    print(db.plainTextTitle)
+                case .failure(let error):
+                    print("\(error.localizedDescription)")
                 }
-            case .failure(let error):
-                print(error)
+                group.leave()
             }
-            group.leave()
+        case .page:
+            NotionAPI.shared.fetchPages { result in
+                group.leave()
+            }
         }
-//        group.enter()
-//        NotionAPI.shared.fetchPages { result in
-//            switch result {
-//            case .success(let pages):
-//                for page in pages.results {
-//                    print(page.plainTextTitle)
-//                }
-//            case .failure(let error):
-//                print(error)
-//            }
-//            group.leave()
-//        }
         group.wait()
     }
 }
+
+
+
+
+//
+//NotionAPI.shared.fetchDatabases { result in
+//    switch result {
+//    case .success(let dbs):
+//        for db in dbs.results {
+//            group.enter()
+//            log(.debug, "notion_api", context: ["action": "fetch_entries", "db": db.plainTextTitle])
+//            NotionAPI.shared.fetchDatabaseEntries(in: db) { result in
+//                switch result {
+//                case .success(let pages):
+//                    for page in pages.results {
+//                        print(page.plainTextTitle)
+//                    }
+//                    if let firstPage = pages.results.first {
+//                        group.enter()
+//                        NotionAPI.shared.fetchPageContent(in: firstPage) { result in
+//                            switch result {
+//                            case .success(let blocks):
+//                                print(blocks)
+//                            case .failure(let error):
+//                                print(error)
+//                            }
+//                            group.leave()
+//                        }
+//                    }
+//                case .failure(let error):
+//                    print(error)
+//                }
+//                group.leave()
+//            }
+//            print(db.plainTextTitle)
+//        }
+//    case .failure(let error):
+//        print(error)
+//    }
+//    group.leave()
+//}
+////        group.enter()
+////        NotionAPI.shared.fetchPages { result in
+////            switch result {
+////            case .success(let pages):
+////                for page in pages.results {
+////                    print(page.plainTextTitle)
+////                }
+////            case .failure(let error):
+////                print(error)
+////            }
+////            group.leave()
+////        }
+//group.wait()
