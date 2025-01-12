a660b27c83db23fa74c15f7dada1e38de40968fe
Adam Wulf <adam@adamwulf.me>
2025-01-12 16:15:45 -0600

    parse the activities, sort per video, and write activities.json to each one.

--- Sources/hunch/Commands/ActivityCommand.swift
+++ Sources/hunch/Commands/ActivityCommand.swift
@@ -33,50 +33,42 @@ struct ActivityCommand: AsyncParsableCommand {
         // Parse activity file
         let activities = try await YouTubeTranscriptKit.getActivity(fileURL: URL(fileURLWithPath: normalizedInputPath))
 
-        // Track earliest/latest dates per video
-        var videoFirstSeen: [String: Date] = [:]
-        var videoLastSeen: [String: Date] = [:]
+        // Filter and group video activities by ID
+        var videoActivities: [String: [(activity: Activity, title: String?)]] = [:]
 
-        // First pass: collect dates
         for activity in activities {
-            if case .video(let id, _) = activity.link {
-                if let firstSeen = videoFirstSeen[id] {
-                    videoFirstSeen[id] = min(firstSeen, activity.timestamp)
-                } else {
-                    videoFirstSeen[id] = activity.timestamp
-                }
-
-                if let lastSeen = videoLastSeen[id] {
-                    videoLastSeen[id] = max(lastSeen, activity.timestamp)
-                } else {
-                    videoLastSeen[id] = activity.timestamp
-                }
+            if case .video(let id, let title) = activity.link {
+                videoActivities[id, default: []].append((activity, title))
             }
         }
 
-        // Second pass: save activities
-        for activity in activities {
-            if case .video(let id, _) = activity.link {
-                let videoPath = (normalizedOutputPath as NSString).appendingPathComponent(id)
-                try fm.createDirectory(atPath: videoPath, withIntermediateDirectories: true)
+        // Sort each video's activities by date
+        for (id, activities) in videoActivities {
+            let sortedActivities = activities.sorted { $0.activity.timestamp < $1.activity.timestamp }
+            videoActivities[id] = sortedActivities
+        }
 
-                // Save activity as JSON
-                let encoder = JSONEncoder()
-                encoder.outputFormatting = [.prettyPrinted]
-                encoder.dateEncodingStrategy = .iso8601
-                let jsonData = try encoder.encode(activity)
-                let jsonPath = (videoPath as NSString).appendingPathComponent("activity.json")
-                try jsonData.write(to: URL(fileURLWithPath: jsonPath))
+        // Process each video
+        for (id, activities) in videoActivities {
+            let videoPath = (normalizedOutputPath as NSString).appendingPathComponent(id)
+            try fm.createDirectory(atPath: videoPath, withIntermediateDirectories: true)
 
-                // Set folder dates
-                if let firstSeen = videoFirstSeen[id], let lastSeen = videoLastSeen[id] {
-                    let attributes: [FileAttributeKey: Any] = [
-                        .creationDate: firstSeen,
-                        .modificationDate: lastSeen
-                    ]
-                    try fm.setAttributes(attributes, ofItemAtPath: videoPath)
-                }
-            }
+            // Save activities as JSON array
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = [.prettyPrinted]
+            encoder.dateEncodingStrategy = .iso8601
+            let jsonData = try encoder.encode(activities.map { $0.activity })
+            let jsonPath = (videoPath as NSString).appendingPathComponent("activities.json")
+            try jsonData.write(to: URL(fileURLWithPath: jsonPath))
+
+            // Set folder dates using first/last activity
+            let firstSeen = activities.first!.activity.timestamp
+            let lastSeen = activities.last!.activity.timestamp
+            let attributes: [FileAttributeKey: Any] = [
+                .creationDate: firstSeen,
+                .modificationDate: lastSeen
+            ]
+            try fm.setAttributes(attributes, ofItemAtPath: videoPath)
         }
     }
 }
