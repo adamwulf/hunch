86a13cb60ceb58cb38a50421ab2ccab6183a4db4
Adam Wulf <adam@adamwulf.me>
2025-01-12 16:06:19 -0600

    Create a directory per video

--- Sources/hunch/Commands/ActivityCommand.swift
+++ Sources/hunch/Commands/ActivityCommand.swift
@@ -33,6 +33,50 @@ struct ActivityCommand: AsyncParsableCommand {
         // Parse activity file
         let activities = try await YouTubeTranscriptKit.getActivity(fileURL: URL(fileURLWithPath: normalizedInputPath))
 
-        print("Found \(activities.count) activities")
+        // Track earliest/latest dates per video
+        var videoFirstSeen: [String: Date] = [:]
+        var videoLastSeen: [String: Date] = [:]
+
+        // First pass: collect dates
+        for activity in activities {
+            if case .video(let id, _) = activity.link {
+                if let firstSeen = videoFirstSeen[id] {
+                    videoFirstSeen[id] = min(firstSeen, activity.timestamp)
+                } else {
+                    videoFirstSeen[id] = activity.timestamp
+                }
+
+                if let lastSeen = videoLastSeen[id] {
+                    videoLastSeen[id] = max(lastSeen, activity.timestamp)
+                } else {
+                    videoLastSeen[id] = activity.timestamp
+                }
+            }
+        }
+
+        // Second pass: save activities
+        for activity in activities {
+            if case .video(let id, _) = activity.link {
+                let videoPath = (normalizedOutputPath as NSString).appendingPathComponent(id)
+                try fm.createDirectory(atPath: videoPath, withIntermediateDirectories: true)
+
+                // Save activity as JSON
+                let encoder = JSONEncoder()
+                encoder.outputFormatting = [.prettyPrinted]
+                encoder.dateEncodingStrategy = .iso8601
+                let jsonData = try encoder.encode(activity)
+                let jsonPath = (videoPath as NSString).appendingPathComponent("activity.json")
+                try jsonData.write(to: URL(fileURLWithPath: jsonPath))
+
+                // Set folder dates
+                if let firstSeen = videoFirstSeen[id], let lastSeen = videoLastSeen[id] {
+                    let attributes: [FileAttributeKey: Any] = [
+                        .creationDate: firstSeen,
+                        .modificationDate: lastSeen
+                    ]
+                    try fm.setAttributes(attributes, ofItemAtPath: videoPath)
+                }
+            }
+        }
     }
 }
